92c3a9102ad578a7f89de038a79dec0775d93e64
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2022-11-11 18:59:29 +0100

    Started Splash Screen

---
+++ app/src/main/java/com/example/hangmanapp/abductmania/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.example.hangmanapp.abductmania
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hangmanapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity()
+{
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
