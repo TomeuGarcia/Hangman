1fd3cda1f5b908d9c4121582691b5b0fdd3e2eb8
Aniol <aniolbarnadat@gmail.com>
2022-11-20 18:34:52 +0100

    Finished register layout & linked it with login (TODO REGISTER LOGIC)

--- app/src/main/java/com/example/hangmanapp/abductmania/RegisterActivity.kt
+++ app/src/main/java/com/example/hangmanapp/abductmania/RegisterActivity.kt
@@ -1,11 +1,28 @@
 package com.example.hangmanapp.abductmania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.hangmanapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.doRegisterButton.setOnClickListener() {
+            // Register new user
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.backLoginButton.setOnClickListener() {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
