ddce41d00a1a031578862157cb552d7915cef87a
tomeu <tomeu.garcia@enti.cat>
2022-12-23 09:46:19 +0100

    Started activity

---
+++ app/src/main/java/com/example/hangmanapp/abductmania/Game/Fragments/HangmanRetryGameFragment.kt
@@ -0,0 +1,23 @@
+package com.example.hangmanapp.abductmania.Game.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class HangmanRetryGameFragment
+    : Fragment()
+{
+
+    // private lateinit var binding :
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
